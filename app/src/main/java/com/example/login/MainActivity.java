c37bd55a74ebe0a7896b26c776b6a8dc68e1f1ae
zhazhahehedian <zhazhahehedian@aliyun.com>
2019-12-15 14:51:27 +0800

    登录1.0

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -8,13 +8,13 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button mBtnTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-            }
-
     }
+
+}
+
+
 
