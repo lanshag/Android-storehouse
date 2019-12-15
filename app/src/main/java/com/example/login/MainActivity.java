30e6362c555d143025a2387ff3a605ea05caade4
EveningShade <1962756480@qq.com>
2019-12-15 13:00:07 +0800

    主键

---
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button mBtnTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+            }
+
+    }
+
