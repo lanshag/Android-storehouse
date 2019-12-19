8f41bc7845a8804a4aa92e10d3d1842f7f7f7164
steve-hahaha <767078385@qq.com>
2019-12-19 22:09:38 +0800

    天气 V2.0

--- app/src/main/java/com/example/login/Weather/WeatherActivity.java
+++ app/src/main/java/com/example/login/Weather/WeatherActivity.java
@@ -1,9 +1,7 @@
 package com.example.login.Weather;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Build;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.webkit.WebChromeClient;
@@ -13,57 +11,42 @@ import android.webkit.WebViewClient;
 
 import com.example.login.R;
 
-
 public class WeatherActivity extends AppCompatActivity {
 
-
-    private WebView mWvMain;
+    // private WebView mWvMain;
+    private WebView mTv_1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
-        mWvMain = findViewById(R.id.Wv_1);
-        //加载本地HTML
-        //mWvMain.loadUrl("file:///android_asset/hello.html");
-        //加载网络HTML,网页打不开就取消注释下面第一条的代码
-        mWvMain.getSettings().setJavaScriptEnabled(true);
-        //始终不离开软件
-//        mWvMain.setWebViewClient(new MyWebVBiewCilent());
-//        mWvMain.setWebChromeClient(new MyWebChromeClient());
-        mWvMain.loadUrl("https://www.heweather.com");
-    }
-//    在软件内继续原网页跳转
-//    class MyWebVBiewCilent extends WebViewClient{
-//        @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-//        @Override
-//        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-//                view.loadUrl(url);
-//            }
-//            return true;
-//        }
-//    }
+        mTv_1 = findViewById(R.id.Wv_1);
+        mTv_1.loadUrl("https://widget-page.heweather.net/h5/index.html?bg=1&md=0123456&lc=auto&key=66216c7af9f84f608042dfeb81e9a00e");
+        mTv_1.getSettings().setJavaScriptEnabled(true);
+        mTv_1.setWebViewClient(new MyWebVBiewCilent());
+        mTv_1.setWebChromeClient(new MyWebChromeClient());}
 
-    // 不离开软件时--返回上一级网页
-//    @Override
-//    public boolean onKeyDown(int keyCode, KeyEvent event) {
-//        if (keyCode == KeyEvent.KEYCODE_BACK && mWvMain.canGoBack()) {
-//            mWvMain.goBack();
-//            return true;
-//        }
-//        return super.onKeyDown(keyCode, event);}
-//
-//        class MyWebChromeClient extends WebChromeClient {
-//            @Override
-//            public void onProgressChanged(WebView view, int newProgress) {
-//                super.onProgressChanged(view, newProgress);
-//            }
-//
-//            @Override
-//            public void onReceivedTitle(WebView view, String title) {
-//                super.onReceivedTitle(view, title);
-//                setTitle(title);
-//            }
-//    }
+    class MyWebVBiewCilent extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            view.loadUrl(request.getUrl().toString());
+            return true;
+        }
+    }
+
+    //         不离开软件时--返回上一级网页
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && mTv_1.canGoBack()) {
+            mTv_1.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);}
+
+    class MyWebChromeClient extends WebChromeClient {
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            super.onProgressChanged(view, newProgress);
+        }
+    }
 }
