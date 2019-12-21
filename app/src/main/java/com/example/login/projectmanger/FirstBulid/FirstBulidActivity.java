476865f564d8f0e8a8826816fb61b3e75742e690
steve-hahaha <767078385@qq.com>
2019-12-21 16:43:17 +0800

    项目管理界面 V1.0

---
+++ app/src/main/java/com/example/login/projectmanger/FirstBulid/FirstBulidActivity.java
@@ -0,0 +1,16 @@
+package com.example.login.projectmanger.FirstBulid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.login.R;
+
+public class FirstBulidActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_bulid);
+    }
+}
