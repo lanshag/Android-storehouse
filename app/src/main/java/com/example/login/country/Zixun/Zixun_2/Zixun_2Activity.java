3edfdab7ba48e138f880b64ad77c230e6d583863
1986392306 <1986392306@qq.com>
2019-12-22 14:42:16 +0800

    美丽乡村1

---
+++ app/src/main/java/com/example/login/country/Zixun/Zixun_2/Zixun_2Activity.java
@@ -0,0 +1,16 @@
+package com.example.login.country.Zixun.Zixun_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.login.R;
+
+public class Zixun_2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zixun_2);
+    }
+}
