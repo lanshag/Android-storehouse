b0c38f01f1a4dc683a0cc1a0238187250ef8107f
2683805554 <2683805554@qq.com>
2019-12-31 04:32:29 +0800

    link实现在app内打开网页

--- app/src/main/java/com/example/login/link/caigouwang/CaigoueangActivity.java
+++ app/src/main/java/com/example/login/link/caigouwang/CaigoueangActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.KeyEvent;
 import android.webkit.WebChromeClient;
 import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -20,34 +21,34 @@ public class CaigoueangActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_caigoueang);
-        mCGW = findViewById(R.id.link_wb_6);
+        init();
+    }
+    private void init() {
+        mCGW = (WebView) findViewById(R.id.link_wb_6);
+
+        WebSettings webSettings = mCGW.getSettings();
+
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        webSettings.setUseWideViewPort(true);//设置webview推荐使用的窗口
+        webSettings.setLoadWithOverviewMode(true);//设置webview加载的页面的模式
+        webSettings.setDisplayZoomControls(false);//隐藏webview缩放按钮
+        webSettings.setJavaScriptEnabled(true); // 设置支持javascript脚本
+        webSettings.setAllowFileAccess(true); // 允许访问文件
+        webSettings.setBuiltInZoomControls(true); // 设置显示缩放按钮
+        webSettings.setSupportZoom(true); // 支持缩放
+        webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
+
+        //WebView加载web资源
         mCGW.loadUrl("http://www.ccgp-hunan.gov.cn/");
-        mCGW.getSettings().setJavaScriptEnabled(true);
-        mCGW.setWebViewClient(new CaigoueangActivity.MyWebVBiewCilent());
-        mCGW.setWebChromeClient(new CaigoueangActivity.MyWebChromeClient());}
-
-    class MyWebVBiewCilent extends WebViewClient {
-        @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-            view.loadUrl(request.getUrl().toString());
-            return true;
-        }
+        //覆盖WebView默认使用第三方或系统默认浏览器打开网页的行为，使网页用WebView打开
+        mCGW.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                //返回值是true的时候控制去WebView打开，为false调用系统浏览器或第三方浏览器
+                view.loadUrl(url);
+                return true;
+            }
+        });
     }
 
-    //         不离开软件时--返回上一级网页
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_BACK && mCGW.canGoBack()) {
-            mCGW.goBack();
-            return true;
-        }
-        return super.onKeyDown(keyCode, event);}
-
-    class MyWebChromeClient extends WebChromeClient {
-        @Override
-        public void onProgressChanged(WebView view, int newProgress) {
-            super.onProgressChanged(view, newProgress);
-        }
-    }
 }
--- app/src/main/java/com/example/login/link/shebao/ShebaoActivity.java
+++ app/src/main/java/com/example/login/link/shebao/ShebaoActivity.java
@@ -11,6 +11,13 @@ import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.example.login.R;
 import com.example.login.link.baidu.BaiduActivity;
@@ -19,38 +26,39 @@ public class ShebaoActivity extends AppCompatActivity {
     private WebView mSB;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shebao);
-        mSB = findViewById(R.id.link_wb_3);
+        init();
+    }
+    private void init() {
+        mSB = (WebView) findViewById(R.id.link_wb_3);
+
+        WebSettings webSettings = mSB.getSettings();
+
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        webSettings.setUseWideViewPort(true);//设置webview推荐使用的窗口
+        webSettings.setLoadWithOverviewMode(true);//设置webview加载的页面的模式
+        webSettings.setDisplayZoomControls(false);//隐藏webview缩放按钮
+        webSettings.setJavaScriptEnabled(true); // 设置支持javascript脚本
+        webSettings.setAllowFileAccess(true); // 允许访问文件
+        webSettings.setBuiltInZoomControls(true); // 设置显示缩放按钮
+        webSettings.setSupportZoom(true); // 支持缩放
+        webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
+
+        //WebView加载web资源
         mSB.loadUrl("http://www.cs12333.com/revision/");
-        mSB.getSettings().setJavaScriptEnabled(true);
-        mSB.setWebViewClient(new ShebaoActivity.MyWebVBiewCilent());
-        mSB.setWebChromeClient(new ShebaoActivity.MyWebChromeClient());}
-    class MyWebVBiewCilent extends WebViewClient {
-        @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-            view.loadUrl(request.getUrl().toString());
-
-            return true;
-        }
+        //覆盖WebView默认使用第三方或系统默认浏览器打开网页的行为，使网页用WebView打开
+        mSB.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                //返回值是true的时候控制去WebView打开，为false调用系统浏览器或第三方浏览器
+                view.loadUrl(url);
+                return true;
+            }
+        });
     }
 
-    //         不离开软件时--返回上一级网页
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_BACK && mSB.canGoBack()) {
-            mSB.goBack();
-            return true;
-        }
-        return super.onKeyDown(keyCode, event);}
-
-    class MyWebChromeClient extends WebChromeClient {
-        @Override
-        public void onProgressChanged(WebView view, int newProgress) {
-            super.onProgressChanged(view, newProgress);
-        }
-    }
 }
