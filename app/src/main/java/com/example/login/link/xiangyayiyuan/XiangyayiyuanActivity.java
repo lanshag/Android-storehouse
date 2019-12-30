0ed283bb9140d2f26fd2d6e2e265273ec89e1032
2683805554 <2683805554@qq.com>
2019-12-30 16:14:44 +0800

    link优化

---
+++ app/src/main/java/com/example/login/link/xiangyayiyuan/XiangyayiyuanActivity.java
@@ -0,0 +1,16 @@
+package com.example.login.link.xiangyayiyuan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.login.R;
+
+public class XiangyayiyuanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_xiangyayiyuan);
+    }
+}
---
+++ app/src/main/java/com/example/login/link/采购网/CaigoueangActivity.java
@@ -0,0 +1,16 @@
+package com.example.login.link.采购网;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.login.R;
+
+public class CaigoueangActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_caigoueang);
+    }
+}
