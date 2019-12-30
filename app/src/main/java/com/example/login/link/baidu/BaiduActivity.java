b5220b2d40d49b9143cb6aaa97150f4782c42060
2683805554 <2683805554@qq.com>
2019-12-31 04:36:24 +0800

    link网页优化：新增缩放功能以及自动适应屏幕

--- app/src/main/java/com/example/login/link/baidu/BaiduActivity.java
+++ app/src/main/java/com/example/login/link/baidu/BaiduActivity.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.view.KeyEvent;
 import android.webkit.WebChromeClient;
 import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -25,33 +26,34 @@ public class BaiduActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_baidu);
         mBaidu = findViewById(R.id.link_wb_5);
+        init();
+    }
+    private void init() {
+        mBaidu = (WebView) findViewById(R.id.link_wb_5);
+
+        WebSettings webSettings = mBaidu.getSettings();
+
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        webSettings.setUseWideViewPort(true);//设置webview推荐使用的窗口
+        webSettings.setLoadWithOverviewMode(true);//设置webview加载的页面的模式
+        webSettings.setDisplayZoomControls(false);//隐藏webview缩放按钮
+        webSettings.setJavaScriptEnabled(true); // 设置支持javascript脚本
+        webSettings.setAllowFileAccess(true); // 允许访问文件
+        webSettings.setBuiltInZoomControls(true); // 设置显示缩放按钮
+        webSettings.setSupportZoom(true); // 支持缩放
+        webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
+
+        //WebView加载web资源
         mBaidu.loadUrl("https://www.baidu.com/");
-        mBaidu.getSettings().setJavaScriptEnabled(true);
-        mBaidu.setWebViewClient(new BaiduActivity.MyWebVBiewCilent());
-        mBaidu.setWebChromeClient(new BaiduActivity.MyWebChromeClient());}
-
-    class MyWebVBiewCilent extends WebViewClient {
-        @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-            view.loadUrl(request.getUrl().toString());
-            return true;
-        }
+        //覆盖WebView默认使用第三方或系统默认浏览器打开网页的行为，使网页用WebView打开
+        mBaidu.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                //返回值是true的时候控制去WebView打开，为false调用系统浏览器或第三方浏览器
+                view.loadUrl(url);
+                return true;
+            }
+        });
     }
 
-    //         不离开软件时--返回上一级网页
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_BACK && mBaidu.canGoBack()) {
-            mBaidu.goBack();
-            return true;
-        }
-        return super.onKeyDown(keyCode, event);}
-
-    class MyWebChromeClient extends WebChromeClient {
-        @Override
-        public void onProgressChanged(WebView view, int newProgress) {
-            super.onProgressChanged(view, newProgress);
-        }
-    }
 }
