ede4a5772c58e7fac65636bf3253c7cee6438320
杨靖鹏 <1615175361@qq.com>
2019-12-31 12:49:08 +0800

    增加返回功能

--- app/src/main/java/com/example/login/iot/jiankong/CCActivity.java
+++ app/src/main/java/com/example/login/iot/jiankong/CCActivity.java
@@ -2,15 +2,30 @@ package com.example.login.iot.jiankong;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.login.R;
+import com.example.login.iot.Button2Activity;
+import com.example.login.iot.huanjingcanshu.EpActivity;
 
 public class CCActivity extends AppCompatActivity {
-
+    private Button mbtnButton2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cc);
+        mbtnButton2 = findViewById(R.id.btn_iot_back);
+        mbtnButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //跳转到Button2演示界面
+                Intent intent = new Intent(CCActivity.this, Button2Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/login/iot/zhongduannengyuan/TemActivity.java
+++ app/src/main/java/com/example/login/iot/zhongduannengyuan/TemActivity.java
@@ -2,15 +2,31 @@ package com.example.login.iot.zhongduannengyuan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.login.R;
+import com.example.login.iot.Button2Activity;
+import com.example.login.iot.huanjingcanshu.EpActivity;
 
 public class TemActivity extends AppCompatActivity {
+    private Button mbtnButton2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tem);
+        mbtnButton2 = findViewById(R.id.btn_iot_back);
+        mbtnButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //跳转到Button2演示界面
+                Intent intent = new Intent(TemActivity.this, Button2Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
