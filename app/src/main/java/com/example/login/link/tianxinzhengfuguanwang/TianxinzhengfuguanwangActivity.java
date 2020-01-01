d4c9be2c7d19f9aa5bbcbd6db60a525a1775e6c3
2683805554 <2683805554@qq.com>
2020-01-01 13:51:05 +0800

    link网页优化：新增返回上一步、后台运行减少所耗内存，退出时销毁等等

--- app/src/main/java/com/example/login/link/tianxinzhengfuguanwang/TianxinzhengfuguanwangActivity.java
+++ app/src/main/java/com/example/login/link/tianxinzhengfuguanwang/TianxinzhengfuguanwangActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.ViewGroup;
 import android.webkit.WebChromeClient;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
@@ -14,6 +15,8 @@ import android.webkit.WebViewClient;
 
 import com.example.login.R;
 import com.example.login.link.baidu.BaiduActivity;
+
+import static android.view.KeyEvent.KEYCODE_BACK;
 
 public class TianxinzhengfuguanwangActivity extends AppCompatActivity {
     private  WebView mTXQ;
@@ -25,21 +28,8 @@ public class TianxinzhengfuguanwangActivity extends AppCompatActivity {
     }
     private void init() {
         mTXQ = (WebView) findViewById(R.id.link_wb_4);
-
-        WebSettings webSettings = mTXQ.getSettings();
-
-        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
-        webSettings.setUseWideViewPort(true);//设置webview推荐使用的窗口
-        webSettings.setLoadWithOverviewMode(true);//设置webview加载的页面的模式
-        webSettings.setDisplayZoomControls(false);//隐藏webview缩放按钮
-        webSettings.setJavaScriptEnabled(true); // 设置支持javascript脚本
-        webSettings.setAllowFileAccess(true); // 允许访问文件
-        webSettings.setBuiltInZoomControls(true); // 设置显示缩放按钮
-        webSettings.setSupportZoom(true); // 支持缩放
-        webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
-
         //WebView加载web资源
-        mTXQ.loadUrl("http://www.tianxin.gov.cn/");
+        mTXQ.loadUrl("http://www.ccgp-hunan.gov.cn/");
         //覆盖WebView默认使用第三方或系统默认浏览器打开网页的行为，使网页用WebView打开
         mTXQ.setWebViewClient(new WebViewClient() {
             @Override
@@ -49,6 +39,66 @@ public class TianxinzhengfuguanwangActivity extends AppCompatActivity {
                 return true;
             }
         });
+        WebSettings webSettings = mTXQ.getSettings();//支持获取手势焦点，输入用户名、密码或其他
+        webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);//支持自动加载图片
+        webSettings.setUseWideViewPort(true);//设置webview推荐使用的窗口
+        webSettings.setLoadWithOverviewMode(true);//设置webview加载的页面的模式
+        webSettings.setAllowFileAccess(true); // 允许访问webview内部文件
+        webSettings.setJavaScriptEnabled(true); // 设置支持javascript脚本
+        webSettings.setSupportZoom(true); //支持缩放，默认为true。是下面那个的前提。
+        webSettings.setBuiltInZoomControls(true); //设置内置的缩放控件。若为false，则该WebView不可缩放
+        webSettings.setDisplayZoomControls(false); //隐藏原生的缩放控件
+
+
+
+
+
+
+
+
+
+        //当应用程序(存在webview)被切换到后台时，这个方法不仅仅针对当前的webview而是全局的全应用程序的webview
+        // 它会暂停所有webview的layout，parsing，javascripttimer。降低CPU功耗。
+        mTXQ.pauseTimers();
+
+
+
+
+
+
+
+
     }
+
+
+
+    //在不做任何处理前提下 ，浏览网页时点击系统的“Back”键,整个 Browser 会调用 finish()而结束自身
+    //目标：点击返回后，是网页回退而不是推出浏览器
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KEYCODE_BACK) && mTXQ.canGoBack()) {
+            mTXQ.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+
+
+    //在 Activity 销毁（ WebView ）的时候，先让 WebView 加载null内容，然后移除 WebView，再销毁 WebView，最后置空。
+    @Override
+    protected void onDestroy() {
+        if (mTXQ != null) {
+            mTXQ.loadDataWithBaseURL(null, "", "text/html", "utf-8", null);
+            mTXQ.clearHistory();
+
+            ((ViewGroup) mTXQ.getParent()).removeView(mTXQ);
+            mTXQ.destroy();
+            mTXQ = null;
+        }
+        super.onDestroy();
+    }
+
+
 
 }
