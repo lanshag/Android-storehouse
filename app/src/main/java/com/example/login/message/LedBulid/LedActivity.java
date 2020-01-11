b4148a8b18dfc635cfbdf7d5c7f02614b6b6784f
jiasongyuan <2421957797@qq.com>
2020-01-11 12:37:56 +0800

    MEssage最后修改

--- app/src/main/java/com/example/login/message/LedBulid/LedActivity.java
+++ app/src/main/java/com/example/login/message/LedBulid/LedActivity.java
@@ -3,6 +3,7 @@ package com.example.login.message.LedBulid;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -19,7 +20,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.widget.TextView;
-
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -32,12 +32,20 @@ public class LedActivity extends AppCompatActivity {
     private Button mbtnLed;
     private Button mBtnButton1;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_led);
         mbtnLed=findViewById(R.id.Ledtn_1);
+
+
+        TextView time1 = (TextView) findViewById(R.id.tv_time1);
+        TextView time2 = (TextView) findViewById(R.id.tv_time2);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss");// HH:mm:ss//获取当前时间
+        Date date = new Date(System.currentTimeMillis());
+        time1.setText(simpleDateFormat.format(date));
+        time2.setText(simpleDateFormat.format(date));
+
         mbtnLed.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -52,11 +60,13 @@ public class LedActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-
     }
 
+
+
 }
+
+
 
 
 
