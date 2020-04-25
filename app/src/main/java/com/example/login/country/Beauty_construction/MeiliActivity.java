274d1115d1f0529a9104328e4b3a86906f3f0865
1986392306 <1986392306@qq.com>
2020-04-25 17:51:13 +0800

    美丽乡村中的乡村管理的轮播图

--- app/src/main/java/com/example/login/country/Beauty_construction/MeiliActivity.java
+++ app/src/main/java/com/example/login/country/Beauty_construction/MeiliActivity.java
@@ -1,22 +1,35 @@
 package com.example.login.country.Beauty_construction;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 import com.example.login.R;
 import com.example.login.country.Beauty_construction.Meili_1.Meili_1Activity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MeiliActivity extends AppCompatActivity {
     private RelativeLayout mRL_1;
+    private ViewPager mViewPager;
+    private List<Integer> mData = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meili);
+        initView();
+        initData();
         mRL_1 = findViewById(R.id.rea_1);
         mRL_1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,5 +38,48 @@ public class MeiliActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
     }
+
+    private void initData() {
+        mData.add(R.mipmap.pic0);
+        mData.add(R.mipmap.pic1);
+        mData.add(R.mipmap.pic2);
+        mPagerAdapter.notifyDataSetChanged();
+    }
+
+    private void initView() {
+        mViewPager = this.findViewById(R.id.view_pager);
+        mViewPager.setAdapter(mPagerAdapter);
+    }
+    private PagerAdapter mPagerAdapter = new PagerAdapter() {
+        @Override
+        public int getCount() {
+            return Integer.MAX_VALUE;
+        }
+
+        @Override
+        public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+            return view ==object;
+        }
+
+        @NonNull
+        @Override
+        public Object instantiateItem(@NonNull ViewGroup container, int position) {
+            View item = LayoutInflater.from(container.getContext()).inflate(R.layout.item_pager,container,false);
+            ImageView iv = item.findViewById(R.id.cover);
+            int realPosition = position % mData.size();
+            iv.setImageResource(mData.get(realPosition));
+            if(iv.getParent() instanceof ViewGroup){
+                ((ViewGroup) iv.getParent()).removeView(iv);
+            }
+            container.addView(iv);
+            return iv;
+        }
+
+        @Override
+        public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+            container.removeView((View)object);
+        }
+    };
 }
